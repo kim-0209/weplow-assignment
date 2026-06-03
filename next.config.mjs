74df1b4daf3b0bd4b742e80dc71b3f8dd9951817
kim-0209 <ktk55594@gmail.com>
2026-06-03 20:37:33 +0900

    remove:전체 제거

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactCompiler: true,
-  allowedDevOrigins: ['192.168.*.*', '10.*.*.*', '172.*.*.*'],
-};
-
-export default nextConfig;
