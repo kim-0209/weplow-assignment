f6df871674425c989c6d1cd38c43ff68a0718dfc
kim-0209 <ktk55594@gmail.com>
2026-06-03 21:21:05 +0900

    feat: vercel 배포 설정 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactCompiler: true,
   allowedDevOrigins: ["192.168.*.*", "10.*.*.*", "172.*.*.*"],
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/weflow-assignment",
 };
 
 export default nextConfig;
